b520ae8c1f48f826086d1454bc750667bc73a2f7
Clarence Albert S. Feliciano <csfeliciano2@up.edu.ph>
2026-07-28 17:49:00 +0800

    Updated 2026 July 28

--- build/CMakeFiles/sim.dir/sim.cc.o.d
+++ build/CMakeFiles/sim.dir/sim.cc.o.d
@@ -684,19 +684,6 @@ CMakeFiles/sim.dir/sim.cc.o: /home/clarence/Geant4-11-tutorial/sim.cc \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VSDFilter.hh \
  /home/clarence/Geant4-11-tutorial/include/PMSensitiveDetector.hh \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VSensitiveDetector.hh \
- /home/clarence/Geant4-11-tutorial/include/PMPrimaryGenerator.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VUserPrimaryGeneratorAction.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleDefinition.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleGun.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VPrimaryGenerator.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleTable.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4Event.hh \
- /home/clarence/Geant4-11-tutorial/include/PMActionInitialization.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VUserActionInitialization.hh \
- /home/clarence/Geant4-11-tutorial/include/PMPrimaryGenerator.hh \
- /home/clarence/Geant4-11-tutorial/include/PMRunAction.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4UserRunAction.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4Run.hh \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4AnalysisManager.hh \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4GenericAnalysisManager.hh \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ToolsAnalysisManager.hh \
@@ -756,4 +743,17 @@ CMakeFiles/sim.dir/sim.cc.o: /home/clarence/Geant4-11-tutorial/sim.cc \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ToolsAnalysisManager.icc \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4GenericAnalysisManager.icc \
  /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4GenericFileManager.hh \
- /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4GenericFileManager.icc
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4GenericFileManager.icc \
+ /home/clarence/Geant4-11-tutorial/include/PMPrimaryGenerator.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VUserPrimaryGeneratorAction.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleDefinition.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleGun.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VPrimaryGenerator.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4ParticleTable.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4Event.hh \
+ /home/clarence/Geant4-11-tutorial/include/PMActionInitialization.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4VUserActionInitialization.hh \
+ /home/clarence/Geant4-11-tutorial/include/PMPrimaryGenerator.hh \
+ /home/clarence/Geant4-11-tutorial/include/PMRunAction.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4UserRunAction.hh \
+ /home/clarence/Software/Geant4/geant4-v11.4.2-install/include/Geant4/G4Run.hh
